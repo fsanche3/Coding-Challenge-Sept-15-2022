6992ca39446d5615947b295cd6676ab46d5ccdfc
fsanche3 <60365292+fsanche3@users.noreply.github.com>
2022-09-16 14:02:50 -0400

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.dev.controllers.test"},{"l":"com.dev.data"},{"l":"com.dev.model"},{"l":"com.dev.models.dtos"},{"l":"com.dev.services.test"},{"l":"com.dev.utils"}];updateSearchResults();
